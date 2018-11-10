9c7e893f2a603485dcdd95d63cdbb44752a6a9d1
rin <souma_rin7th@yahoo.co.jp>
2018-11-10 13:05:48 +0900

    結果は日替わり

--- app/controllers/oracle_controller.rb
+++ app/controllers/oracle_controller.rb
@@ -1,10 +1,9 @@
 class OracleController < ApplicationController
   def index
     ip_address = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip
-    md5 = Digest::MD5.hexdigest(ip_address)
+    md5 = Digest::MD5.hexdigest(ip_address << Date.today.to_s)
     @rnd = md5.slice(0, 3).hex % $IMG_LIST.length
     @img_name = $IMG_LIST[@rnd]
     @description_msg = $DES_LIST[@rnd]
-    p @rnd
   end
 end
