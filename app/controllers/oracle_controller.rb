17d9c72c3a73675a76ea5b6595cb9fc67f039699
rin <souma_rin7th@yahoo.co.jp>
2018-11-10 13:01:22 +0900

    ツイート用URLを修正

--- app/controllers/oracle_controller.rb
+++ app/controllers/oracle_controller.rb
@@ -1,9 +1,10 @@
 class OracleController < ApplicationController
   def index
     ip_address = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip
-    @md5 = Digest::MD5.hexdigest(ip_address)
-    rnd = @md5.slice(0, 3).hex % $IMG_LIST.length
-    @img_name = $IMG_LIST[rnd]
-    @description_msg = $DES_LIST[rnd]
+    md5 = Digest::MD5.hexdigest(ip_address)
+    @rnd = md5.slice(0, 3).hex % $IMG_LIST.length
+    @img_name = $IMG_LIST[@rnd]
+    @description_msg = $DES_LIST[@rnd]
+    p @rnd
   end
 end
